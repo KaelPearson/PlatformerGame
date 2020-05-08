0a1c44ce8e58976eeaa8d8ebdf61629d92db2996
KaelPearson <kaelpearson@gmail.com>
2020-05-08 03:33:53 -0700

    Fuck you tyler

---
+++ Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch : MonoBehaviour
+{
+    public string level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.tag == "Player"){
+            SceneManager.LoadScene(level);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
