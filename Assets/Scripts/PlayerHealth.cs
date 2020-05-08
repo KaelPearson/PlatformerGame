e1b313ef1c4b151f39755796414227f33b789c0f
KaelPearson <kaelpearson@gmail.com>
2020-05-08 03:12:47 -0700

    Added death

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,28 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public CanvasGroup canvasGroup;
     public Vector3 spawnPosition;
+    bool death = false;
+    float time = 0;
     // Start is called before the first frame update
     void Start()
     {
-        spawnPosition = transform.position;
+        spawnPosition = new Vector3(transform.position.x, transform.position.y, 0);
     }
 
     void OnTriggerEnter2D(Collider2D col){
 
-        if(col.tag == "BottomOfMap"){
-            die();
+        if(col.tag == "Death"){
+            death = true;
         }
     }
     void die(){
+        float time = Time.time + 5;
+        while(time > 0){
+            time -= Time.deltaTime;
+        }
+        
+        canvasGroup.alpha = 0f;
+        canvasGroup.blocksRaycasts = false;
         transform.position = spawnPosition;
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if(death == true){
+            canvasGroup.alpha = 1f;
+            canvasGroup.blocksRaycasts = true;
+            transform.position = spawnPosition;
+            if(time > 5){  
+                death = false;
+                canvasGroup.alpha = 0f;
+                canvasGroup.blocksRaycasts = false;
+                transform.position = spawnPosition;
+                time = 0;
+            }
+            time += Time.deltaTime;
+        }
     }
 }
